41be63aca9a328ef16cf037956fd6fcdb07ccfca
Chad Christensen <chadchristensen2@gmail.com>
2024-09-26 22:58:05 -0400

    create initial navbar component markup

--- client/src/app/meals/page.tsx
+++ client/src/app/meals/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function MealsPage() {
   return (
-    <>
-      <h1>Meals</h1>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Meals</h1>
       <Link href={"/meals/create"}>Create new meal</Link>
-    </>
+    </div>
   );
 }
